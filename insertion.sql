7caedabc6174861822d0cc8473285bcfb50d3851
hejl <hejl20150101@gmail.com>
2021-04-02 15:16:51 +0800

    Add Customers test cases

---
+++ insertion.sql
@@ -0,0 +1,23 @@
+-------- Insert Customers
+
+-- Positive test cases
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1676030602899','Nell T. Calderon','76044067','Nunc.lectus@orci.ca','2447 Enim. Avenue');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1670121565299','Merritt C. Wagner','61281669','tristique.senectus.et@luctuslobortisClass.org','P.O. Box 752, 5557 Neque Av.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1602032836299','Adara M. Bailey','89253939','augue.scelerisque@mauris.net','Ap #329-1731 Et Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1678011764099','Adara M. Bailey','89686053','a@aliquameuaccumsan.edu','P.O. Box 105, 9504 Tristique St.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1655120277499','Oren C. Byrd','30999101','mi.felis.adipiscing@Fusce.ca','3840 Mauris Street');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1682121904099','Brian Z. Osborn','30999101','ut.nulla@amalesuadaid.com','241-1798 Fusce Av.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1681120138899','James L. Barrett','13238542','tempor.arcu@mauriselit.com','P.O. Box 590, 5859 Quam Av.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1640120779699','Abbot C. Woods','93308560','adipiscing@augueeutellus.com','878-1689 Natoque Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1625072041899','Burton O. Langley','24345387','Aliquam@ut.ca','878-1689 Natoque Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1623012646899','Dorothy K. Nguyen','22745671','a@ullamcorper.net','Ap #365-1251 Pede Ave');
+
+-- Negative test cases
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1650112335899','Conan H. Hewitt','83453879','lorem@nonarcu.edu','P.O. Box 159, 6870 Fusce Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1650112335899','John Z. Witt','37291142','blandit.Nam.nulla@nonfeugiat.ca','8822 Magna. Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (NULL,'Cameron T. Castillo','92187324','lacinia@eget.org','224-9477 Vitae Road');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1677083036799',NULL,'36111914','neque.Nullam.nisl@dictum.ca','793 Tristique Avenue');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1683022040099','Raya Y. Tate',NULL,'est.Mauris.eu@lectus.edu','Ap #445-295 Ullamcorper St.');
+
+
+
