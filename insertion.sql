44e47e9ea6701fe6d934bc7b0ec3bd0011314698
hejl <hejl20150101@gmail.com>
2021-04-02 18:05:57 +0800

    Correct syntax error

--- insertion.sql
+++ insertion.sql
@@ -2,43 +2,43 @@
 -------- Insert Customers
 
 -- Positve insertions for Employees hierarchy
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1,'Nell T. Calderon',76044067,'Nunc.lectus@orci.ca','2447 Enim. Avenue');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (2,'Merritt C. Wagner',61281669,'tristique.senectus.et@luctuslobortisClass.org','P.O. Box 752, 5557 Neque Av.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (3,'Adara M. Bailey',89253939,'augue.scelerisque@mauris.net','Ap #329-1731 Et Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (4,'Adara M. Bailey',89686053,'a@aliquameuaccumsan.edu','P.O. Box 105, 9504 Tristique St.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (5,'Oren C. Byrd',30999101,'mi.felis.adipiscing@Fusce.ca','3840 Mauris Street');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (6,'Brian Z. Osborn',30999101,'ut.nulla@amalesuadaid.com','241-1798 Fusce Av.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (7,'James L. Barrett',1323854,'tempor.arcu@mauriselit.com','P.O. Box 590, 5859 Quam Av.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (8,'Abbot C. Woods',93308560,'adipiscing@augueeutellus.com','878-1689 Natoque Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (9,'Burton O. Langley',24345387,'Aliquam@ut.ca','878-1689 Natoque Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (10,'Dorothy K. Nguyen',22745671,'a@ullamcorper.net','Ap #365-1251 Pede Ave');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (1,'Nell T. Calderon',76044067,'Nunc.lectus@orci.ca','2447 Enim. Avenue');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (2,'Merritt C. Wagner',61281669,'tristique.senectus.et@luctuslobortisClass.org','P.O. Box 752, 5557 Neque Av.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (3,'Adara M. Bailey',89253939,'augue.scelerisque@mauris.net','Ap #329-1731 Et Rd.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (4,'Adara M. Bailey',89686053,'a@aliquameuaccumsan.edu','P.O. Box 105, 9504 Tristique St.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (5,'Oren C. Byrd',30999101,'mi.felis.adipiscing@Fusce.ca','3840 Mauris Street');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (6,'Brian Z. Osborn',30999101,'ut.nulla@amalesuadaid.com','241-1798 Fusce Av.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (7,'James L. Barrett',1323854,'tempor.arcu@mauriselit.com','P.O. Box 590, 5859 Quam Av.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (8,'Abbot C. Woods',93308560,'adipiscing@augueeutellus.com','878-1689 Natoque Rd.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (9,'Burton O. Langley',24345387,'Aliquam@ut.ca','878-1689 Natoque Rd.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (10,'Dorothy K. Nguyen',22745671,'a@ullamcorper.net','Ap #365-1251 Pede Ave');
 
 -- Negative insertions for Employees hierarchy
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (11,'Conan H. Hewitt',83453879,'lorem@nonarcu.edu','P.O. Box 159, 6870 Fusce Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (11,'John Z. Witt',37291142,'blandit.Nam.nulla@nonfeugiat.ca','8822 Magna. Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (NULL,'Cameron T. Castillo',92187324,'lacinia@eget.org','224-9477 Vitae Road');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (11,'Conan H. Hewitt',83453879,'lorem@nonarcu.edu','P.O. Box 159, 6870 Fusce Rd.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (11,'John Z. Witt',37291142,'blandit.Nam.nulla@nonfeugiat.ca','8822 Magna. Rd.');
+INSERT INTO Customers (cust_id,name,phone,email,address) VALUES (NULL,'Cameron T. Castillo',92187324,'lacinia@eget.org','224-9477 Vitae Road');
 
 
 -------- Insert Rooms
 
 -- Positve insertions for Rooms
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (09948492199,'#06-013, P.O. Box 919, 1276 Nunc Rd.',151);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (25393426299,'P.O. Box 518, 3075 Ipsum. Av.',132);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (45401285099,'Ap #219-5134 Consequat Ave',16);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (06318568699,'604-1129 Convallis Ave',53);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (77228092599,'Ap #221-9496 Cursus Road',119);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (20600578799,'2075 Ligula. Rd.',27);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (71803288599,'445-4464 Ipsum Av.',55);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (08474057799,'P.O. Box 152, 9645 Nec Street',190);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (09397611499,'646-2248 Nisi. Av.',99);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (61384650999,'Ap #601-7100 Ipsum. St.',41);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (09948492199,'#06-013, P.O. Box 919, 1276 Nunc Rd.',151);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (25393426299,'P.O. Box 518, 3075 Ipsum. Av.',132);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (45401285099,'Ap #219-5134 Consequat Ave',16);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (06318568699,'604-1129 Convallis Ave',53);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (77228092599,'Ap #221-9496 Cursus Road',119);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (20600578799,'2075 Ligula. Rd.',27);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (71803288599,'445-4464 Ipsum Av.',55);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (08474057799,'P.O. Box 152, 9645 Nec Street',190);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (09397611499,'646-2248 Nisi. Av.',99);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (61384650999,'Ap #601-7100 Ipsum. St.',41);
 
 -- Negative insertions for Rooms
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (NULL,'P.O. Box 242, 3441 Magna Rd.',129);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (11621146999,'P.O. Box 656, 6025 Mauris St.',160);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (11621146999,'711-8257 Duis Avenue',200);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (65705231899,NULL,53);
-INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (13300765899,'P.O. Box 718, 3543 Ligula. Rd.',NULL);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (NULL,'P.O. Box 242, 3441 Magna Rd.',129);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (11621146999,'P.O. Box 656, 6025 Mauris St.',160);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (11621146999,'711-8257 Duis Avenue',200);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (65705231899,NULL,53);
+INSERT INTO Rooms (rid,location,seating_capacity) VALUES (13300765899,'P.O. Box 718, 3543 Ligula. Rd.',NULL);
 
 
 -------- Insert Course_areas
@@ -87,23 +87,23 @@ INSERT INTO Course_packages (package_id, sale_start_date, num_free_registrations
 -------- Insert Credit_cards
 
 -- Positve insertions for Credit_cards
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (5207757125861655, 712,'21-12-21', 1676030602899);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (1449389814987739, 448,'21-01-08', 1670121565299);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (4772425519204232, 468,'21-09-28', 1602032836299);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (2238406833054283, 132,'20-12-03', 1678011764099);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (8405238874121091, 906,'21-07-18', 1655120277499);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (5404411412295264, 320,'20-06-22', 1682121904099);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (7268272469910540, 834,'22-02-27', 1681120138899);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (7531491645485563, 258,'20-09-10', 1640120779699);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (5204373474155087, 442,'20-09-09', 1625072041899);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (2065629961409359, 377,'20-05-08', 1623012646899);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (5207757125861655, 712,'21-12-21', 1676030602899);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (1449389814987739, 448,'21-01-08', 1670121565299);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (4772425519204232, 468,'21-09-28', 1602032836299);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (2238406833054283, 132,'20-12-03', 1678011764099);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (8405238874121091, 906,'21-07-18', 1655120277499);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (5404411412295264, 320,'20-06-22', 1682121904099);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (7268272469910540, 834,'22-02-27', 1681120138899);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (7531491645485563, 258,'20-09-10', 1640120779699);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (5204373474155087, 442,'20-09-09', 1625072041899);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (2065629961409359, 377,'20-05-08', 1623012646899);
 
 -- Negative insertions for Credit_cards
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (NULL, 831,'20-08-29', 1623012623456);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (3551375609223241, 547,1623765436899);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (3551375609223241, 641,'20-08-30', 1623012609865);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (1326261219057330, 368,'21-09-17', NULL);
-INSERT INTO "Credit_cards" (number, CVV, expiry_date, cust_id, from_date) VALUES (9612070681780448, 438,'20-12-22', 1633417626899);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (NULL, 831,'20-08-29', 1623012623456);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (3551375609223241, 547,1623765436899);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (3551375609223241, 641,'20-08-30', 1623012609865);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (1326261219057330, 368,'21-09-17', NULL);
+INSERT INTO Credit_cards (number, CVV, expiry_date, cust_id, from_date) VALUES (9612070681780448, 438,'20-12-22', 1633417626899);
 
 
 -------- Insert Courses
