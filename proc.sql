fbb31d0fd527c20230095e78557f38b800cec990
Xue Yong <xueyong26@hotmail.com>
2021-04-02 16:59:40 +0800

    Add update_credit_card

--- proc.sql
+++ proc.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE ROUTINE add_course()
 
 
 
-
+/* 3 */
 CREATE OR REPLACE FUNCTION add_customer(cname TEXT, caddress TEXT, cphone INTEGER, cemail TEXT, cnumber INTEGER, cexpiry_date DATE, ccvv INTEGER)
 	RETURNS VOID 
 AS $$
@@ -14,5 +14,15 @@ BEGIN
     VALUES (cname, caddress, cphone, cemail) RETURNING cust_id INTO cid;
 	INSERT INTO Credit_cards(number, expiry_date, CVV, cust_id)
 	VALUES (cnumber, cexpiry_date, ccvv, cid);
+END;
+$$ LANGUAGE plpgsql;
+
+/* 4 */
+CREATE OR REPLACE FUNCTION update_credit_card(cid INTEGER, cnumber INTEGER, cexpiry_date DATE, ccvv INTEGER)
+	RETURNS VOID 
+AS $$
+BEGIN
+	INSERT INTO Credit_cards(number, expiry_date, CVV, cust_id)
+	VALUES (cid, cexpiry_date, ccvv, cid);
 END;
 $$ LANGUAGE plpgsql;
