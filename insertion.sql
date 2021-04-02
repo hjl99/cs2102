3307928e2c0fb909aee5b3867f962352ee753496
hejl <hejl20150101@gmail.com>
2021-04-02 15:41:07 +0800

    Add test cases for Rooms

--- insertion.sql
+++ insertion.sql
@@ -1,23 +1,47 @@
 -------- Insert Customers
 
 -- Positve insertions for Employees hierarchy
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1676030602899','Nell T. Calderon','76044067','Nunc.lectus@orci.ca','2447 Enim. Avenue');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1670121565299','Merritt C. Wagner','61281669','tristique.senectus.et@luctuslobortisClass.org','P.O. Box 752, 5557 Neque Av.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1602032836299','Adara M. Bailey','89253939','augue.scelerisque@mauris.net','Ap #329-1731 Et Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1678011764099','Adara M. Bailey','89686053','a@aliquameuaccumsan.edu','P.O. Box 105, 9504 Tristique St.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1655120277499','Oren C. Byrd','30999101','mi.felis.adipiscing@Fusce.ca','3840 Mauris Street');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1682121904099','Brian Z. Osborn','30999101','ut.nulla@amalesuadaid.com','241-1798 Fusce Av.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1681120138899','James L. Barrett','13238542','tempor.arcu@mauriselit.com','P.O. Box 590, 5859 Quam Av.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1640120779699','Abbot C. Woods','93308560','adipiscing@augueeutellus.com','878-1689 Natoque Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1625072041899','Burton O. Langley','24345387','Aliquam@ut.ca','878-1689 Natoque Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1623012646899','Dorothy K. Nguyen','22745671','a@ullamcorper.net','Ap #365-1251 Pede Ave');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1676030602899,'Nell T. Calderon',76044067,'Nunc.lectus@orci.ca','2447 Enim. Avenue');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1670121565299,'Merritt C. Wagner',61281669,'tristique.senectus.et@luctuslobortisClass.org','P.O. Box 752, 5557 Neque Av.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1602032836299,'Adara M. Bailey',89253939,'augue.scelerisque@mauris.net','Ap #329-1731 Et Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1678011764099,'Adara M. Bailey',89686053,'a@aliquameuaccumsan.edu','P.O. Box 105, 9504 Tristique St.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1655120277499,'Oren C. Byrd',30999101,'mi.felis.adipiscing@Fusce.ca','3840 Mauris Street');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1682121904099,'Brian Z. Osborn',30999101,'ut.nulla@amalesuadaid.com','241-1798 Fusce Av.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1681120138899,'James L. Barrett',1323854,'tempor.arcu@mauriselit.com','P.O. Box 590, 5859 Quam Av.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1640120779699,'Abbot C. Woods',93308560,'adipiscing@augueeutellus.com','878-1689 Natoque Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1625072041899,'Burton O. Langley',24345387,'Aliquam@ut.ca','878-1689 Natoque Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1623012646899,'Dorothy K. Nguyen',22745671,'a@ullamcorper.net','Ap #365-1251 Pede Ave');
 
 -- Negative insertions for Employees hierarchy
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1650112335899','Conan H. Hewitt','83453879','lorem@nonarcu.edu','P.O. Box 159, 6870 Fusce Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1650112335899','John Z. Witt','37291142','blandit.Nam.nulla@nonfeugiat.ca','8822 Magna. Rd.');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (NULL,'Cameron T. Castillo','92187324','lacinia@eget.org','224-9477 Vitae Road');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1677083036799',NULL,'36111914','neque.Nullam.nisl@dictum.ca','793 Tristique Avenue');
-INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES ('1683022040099','Raya Y. Tate',NULL,'est.Mauris.eu@lectus.edu','Ap #445-295 Ullamcorper St.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1650112335899,'Conan H. Hewitt',83453879,'lorem@nonarcu.edu','P.O. Box 159, 6870 Fusce Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1650112335899,'John Z. Witt',37291142,'blandit.Nam.nulla@nonfeugiat.ca','8822 Magna. Rd.');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (NULL,'Cameron T. Castillo',92187324,'lacinia@eget.org','224-9477 Vitae Road');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1677083036799,NULL,36111914,'neque.Nullam.nisl@dictum.ca','793 Tristique Avenue');
+INSERT INTO "Customers" (cust_id,name,phone,email,address) VALUES (1683022040099,'Raya Y. Tate',NULL,'est.Mauris.eu@lectus.edu','Ap #445-295 Ullamcorper St.');
 
 
+-------- Insert Rooms
 
+-- Positve insertions for Rooms
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (09948492199,'#06-013, P.O. Box 919, 1276 Nunc Rd.',151);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (25393426299,'P.O. Box 518, 3075 Ipsum. Av.',132);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (45401285099,'Ap #219-5134 Consequat Ave',16);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (06318568699,'604-1129 Convallis Ave',53);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (77228092599,'Ap #221-9496 Cursus Road',119);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (20600578799,'2075 Ligula. Rd.',27);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (71803288599,'445-4464 Ipsum Av.',55);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (08474057799,'P.O. Box 152, 9645 Nec Street',190);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (09397611499,'646-2248 Nisi. Av.',99);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (61384650999,'Ap #601-7100 Ipsum. St.',41);
+
+-- Negative insertions for Rooms
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (NULL,'P.O. Box 242, 3441 Magna Rd.',129);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (11621146999,'P.O. Box 656, 6025 Mauris St.',160);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (11621146999,'711-8257 Duis Avenue',200);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (65705231899,NULL,53);
+INSERT INTO "Rooms" (rid,location,seating_capacity) VALUES (13300765899,'P.O. Box 718, 3543 Ligula. Rd.',NULL);
+
+
+-------- Insert Course_areas
+
+-- Positve insertions for Course_areas
