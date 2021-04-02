45086067ba67ce9e857d6a8d3a615bf4db9e8871
Xue Yong <xueyong26@hotmail.com>
2021-04-02 16:49:24 +0800

    Add add_customer function

--- proc.sql
+++ proc.sql
@@ -4,9 +4,15 @@ CREATE OR REPLACE ROUTINE add_course()
 
 
 
-CREATE OR REPLACE PROCEDURE add_customer(name TEXT, address TEXT, phone INTEGER, email TEXT, number INTEGER, expiry_date DATE, cvv INTEGER)
-RETURNS VOID AS $$
-    INSERT INTO Customers(name, address, phone, email)
-    VALUES (name, address, phone, email);
-	INSERT INTO Credit_cards(number, expiry_date, CVV, cust_id, from_date)
-	VALUES (number, expiry_date, cvv, )
+CREATE OR REPLACE FUNCTION add_customer(cname TEXT, caddress TEXT, cphone INTEGER, cemail TEXT, cnumber INTEGER, cexpiry_date DATE, ccvv INTEGER)
+	RETURNS VOID 
+AS $$
+DECLARE 
+	cid INTEGER;
+BEGIN
+    INSERT INTO Customers (name, address, phone, email)
+    VALUES (cname, caddress, cphone, cemail) RETURNING cust_id INTO cid;
+	INSERT INTO Credit_cards(number, expiry_date, CVV, cust_id)
+	VALUES (cnumber, cexpiry_date, ccvv, cid);
+END;
+$$ LANGUAGE plpgsql;
