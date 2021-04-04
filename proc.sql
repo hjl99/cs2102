ae48a40e86f32466c9a7900447030dc3b3d614d6
patricktan6 <tanxizhe@gmail.com>
2021-04-05 01:09:25 +0800

    Modified remove_employee routine

--- proc.sql
+++ proc.sql
@@ -86,10 +86,6 @@ AS $$
 		THEN RAISE EXCEPTION 'Employee cannot be removed!';
 		ELSE
 			UPDATE Employees E SET E.depart_date = depart_date WHERE E.eid = reid;
-			-- remove the employee from Part Time Employees, Full Time Employees and Instructors tables
-			DELETE FROM Part_time_emp P WHERE P.eid = reid;
-			DELETE FROM Full_time_emp F WHERE F.eid = reid;
-			DELETE FROM Instructors I WHERE I.eid = reid;
 		END IF;
 	END;
 $$ LANGUAGE plpgsql;
