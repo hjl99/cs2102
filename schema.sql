d005539e1e9def0668cca8da33972ca520cddf69
Trav1sT <travtoh@gmail.com>
2021-04-08 00:29:53 +0800

    Add not null to schema

--- schema.sql
+++ schema.sql
@@ -8,23 +8,23 @@ Cancels, Buys, Registers, Specializes, Redeems, Conducts CASCADE;
  --<----------------------- company side ----------------------->
 CREATE TABLE Employees (
     eid SERIAL PRIMARY KEY,
-    name TEXT,
+    name TEXT NOT NULL,
     phone INTEGER,
     email TEXT,
-    join_date DATE,
+    join_date DATE NOT NULL DEFAULT CURRENT_DATE,
     address TEXT,
     depart_date DATE
 );
 
 CREATE TABLE Part_time_emp (
     eid INTEGER PRIMARY KEY REFERENCES Employees ON DELETE CASCADE,
-    hourly_rate FLOAT
+    hourly_rate FLOAT NOT NULL
     CONSTRAINT hourly_rate_non_neg CHECK (hourly_rate >= 0.0)
 );
 
 CREATE TABLE Full_time_emp (
     eid INTEGER PRIMARY KEY REFERENCES Employees ON DELETE CASCADE,
-    monthly_salary FLOAT
+    monthly_salary FLOAT NOT NULL
     CONSTRAINT monthly_salary_non_neg CHECK (monthly_salary >= 0.0)
 );
 
@@ -53,7 +53,7 @@ CREATE TABLE Managers (
 CREATE TABLE Pay_slips (
     eid INTEGER REFERENCES Employees ON DELETE CASCADE,
     payment_date DATE,
-    amt FLOAT,
+    amt FLOAT NOT NULL,
     num_work_hours INTEGER,
     num_work_days INTEGER,
     PRIMARY KEY (eid, payment_date),
@@ -65,7 +65,7 @@ CREATE TABLE Pay_slips (
 
 CREATE TABLE Customers (
     cust_id SERIAL PRIMARY KEY,
-    cust_name TEXT,
+    cust_name TEXT NOT NULL,
     phone INTEGER,
     email TEXT,
     address TEXT
@@ -73,19 +73,19 @@ CREATE TABLE Customers (
 
 CREATE TABLE Rooms (
     rid SERIAL PRIMARY KEY,
-    location TEXT,
-    seating_capacity INTEGER
+    location TEXT NOT NULL,
+    seating_capacity INTEGER DEFAULT 0
     CONSTRAINT seating_capacity_pos CHECK (seating_capacity > 0)
 );
 
 
 CREATE TABLE Course_packages (
     package_id SERIAL PRIMARY KEY,
-    sale_start_date DATE,
-    num_free_registrations INTEGER,
-    sale_end_date DATE,
+    sale_start_date DATE NOT NULL,
+    num_free_registrations INTEGER NOT NULL,
+    sale_end_date DATE NOT NULL,
     package_name TEXT,
-    price FLOAT 
+    price FLOAT NOT NULL
     CONSTRAINT price_non_neg CHECK (price >= 0.0),
     CONSTRAINT sale_date_validity CHECK (sale_start_date <= sale_end_date),
     CONSTRAINT num_free_reg_pos CHECK (num_free_registrations > 0)
@@ -98,9 +98,9 @@ CREATE TABLE Course_areas (
 
 CREATE TABLE Courses (
     course_id SERIAL PRIMARY KEY,
-    duration INTEGER,
+    duration INTEGER NOT NULL DEFAULT 1,
     description TEXT,
-    title TEXT UNIQUE,
+    title TEXT UNIQUE NOT NULL,
     course_area_name TEXT NOT NULL REFERENCES Course_areas,
     CONSTRAINT duration_validity CHECK (duration > 0 and duration <= 4)
 );
@@ -108,12 +108,12 @@ CREATE TABLE Courses (
 CREATE TABLE Offerings (
     course_id INTEGER REFERENCES Courses ON DELETE CASCADE,
     launch_date DATE,
-    start_date DATE,
-    end_date DATE,
-    reg_deadline DATE,
-    num_target_reg INTEGER,
-    seating_capacity INTEGER,
-    fees FLOAT,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    reg_deadline DATE NOT NULL,
+    num_target_reg INTEGER NOT NULL,
+    seating_capacity INTEGER NOT NULL,
+    fees FLOAT NOT NULL,
     eid INTEGER NOT NULL REFERENCES Administrators,
     PRIMARY KEY (course_id, launch_date),   
     CONSTRAINT start_end_date_validity CHECK (start_date <= end_date),
@@ -123,9 +123,9 @@ CREATE TABLE Offerings (
 
 CREATE TABLE Sessions (
     sid INTEGER,
-    s_date DATE,
-    start_time TIME,
-    end_time TIME,
+    s_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
     course_id INTEGER,
     launch_date DATE,
     rid INTEGER NOT NULL REFERENCES Rooms ON DELETE CASCADE deferrable initially immediate,
@@ -140,14 +140,14 @@ CREATE TABLE Sessions (
 
 -- <----------------------associations----------------------->
 CREATE TABLE Cancels (
-    c_date DATE,
+    c_date DATE NOT NULL DEFAULT CURRENT_DATE,
     refund_amt FLOAT,
     package_credit INTEGER,
     cust_id INTEGER REFERENCES Customers ON DELETE NO ACTION,
     course_id INTEGER,
     launch_date DATE,
     sid INTEGER,
-    payment_date DATE,
+    payment_date DATE NOT NULL,
     FOREIGN KEY (sid, course_id, launch_date) REFERENCES Sessions ON DELETE SET NULL, /* for book keeping purposes */
     PRIMARY KEY (c_date, cust_id, course_id, launch_date, sid),
     CONSTRAINT cancellation_validity CHECK ((refund_amt >= 0.0 and package_credit = NULL) or (package_credit in (0, 1) and refund_amt = NULL))
@@ -156,8 +156,8 @@ CREATE TABLE Cancels (
 /* Contains the owns relationship to enforce key and total participation on credit cards */
 CREATE TABLE Credit_cards (
     number INTEGER PRIMARY KEY,
-    CVV INTEGER,
-    expiry_date DATE,
+    CVV INTEGER NOT NULL,
+    expiry_date DATE NOT NULL,
     cust_id INTEGER NOT NULL REFERENCES Customers ON DELETE CASCADE,
     from_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -167,7 +167,7 @@ CREATE TABLE Buys (
     package_id INTEGER REFERENCES Course_packages ON DELETE SET NULL, 
     number INTEGER REFERENCES Credit_cards ON DELETE CASCADE,
     b_date DATE DEFAULT CURRENT_DATE,
-    num_remaining_redemptions INTEGER,
+    num_remaining_redemptions INTEGER NOT NULL,
     PRIMARY KEY (package_id, number, b_date),
     CONSTRAINT num_remaining_redemptions_non_neg CHECK (num_remaining_redemptions >= 0)
 );
